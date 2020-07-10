50587c00805ce3b95a258f1cee7d44eeba3a9d0c
newmanshee <newman_shee@hotmail.com>
2020-07-10 14:51:53 -0400

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -29,7 +29,7 @@ module.exports = {
             if (ksoft_token == null)
               console.log("failed to get ksoft_token");
             else
-              console.log("found ksoft_token "  + ksoft_token.substring(0,3));
+              console.log("found ksoft_token=###"  + ksoft_token.substring(0,3) + "###");
             const ksoft = new KSoftClient(ksoft_token);
             if (ksoft == null)
             { 
